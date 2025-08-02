0f0ac418600469442e216b6005361bc972a68154
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-02 05:41:29 +0100

    Fix css style

--- static-react/src/components/Card.jsx
+++ static-react/src/components/Card.jsx
@@ -1,29 +1,23 @@
 export default function Card() {
   return (
     <div className="card">
-      <div className="card-content">
-        <div className="cloud-container">
-          <div className="cloud-filter"></div>
-          <div className="cloud-content">
-            <h1>Rudex J. Tede</h1>
-            <p>React Developer with a passion for building web applications.</p>
-            <div className="card-footer">
-              <div className="card-rang">
-                <div>
-                  <span class="material-symbols-outlined">
-                    contacts_product
-                  </span>
-                  <span>312</span>
-                </div>
-                <div>
-                  <span class="material-symbols-outlined">
-                    library_add_check
-                  </span>
-                  <span>100</span>
-                </div>
+      <div className="card-content cloud-container">
+        <div className="cloud-filter"></div>
+        <div className="cloud-content">
+          <h1>Rudex J. Tede</h1>
+          <p>React Developer with a passion for building web applications.</p>
+          <div className="card-footer">
+            <div className="card-rang">
+              <div>
+                <span class="material-symbols-outlined">contacts_product</span>
+                <span>312</span>
               </div>
-              <button>Follow +</button>
+              <div>
+                <span class="material-symbols-outlined">library_add_check</span>
+                <span>100</span>
+              </div>
             </div>
+            <button>Follow +</button>
           </div>
         </div>
       </div>
