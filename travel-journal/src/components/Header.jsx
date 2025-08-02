dcf850b58d1a73d3d9f2455962fde568167eb5f4
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-02 07:02:18 +0100

    Started new challenge: create header component for my travel journal

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="src/assets/image.png" alt="travel journal logo" />
+            <p className="site-title">my travel journal</p>
+        </header>
+    )
+}
