e28bb2908a426d25999ff20e6da8035bba24fb33
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-02 06:22:44 +0100

    Completed project: Create a digital business card

--- static-react/src/components/Card.jsx
+++ static-react/src/components/Card.jsx
@@ -4,17 +4,26 @@ export default function Card() {
       <div className="card-content cloud-container">
         <div className="cloud-filter"></div>
         <div className="cloud-content">
-          <h1 className="card-title">Rudex J. Tede</h1>
-          <p className="card-description">React Developer with a passion for building web applications.</p>
+          <h1 className="card-title">
+            Rudex J. Tede
+            <i class="material-icons">verified</i>
+          </h1>
+          <p className="card-description">
+            React Developer with a passion for building web applications.
+          </p>
           <div className="card-footer">
             <div className="card-rang">
               <div className="rang">
-                <span class="material-symbols-outlined">contacts_product</span>
-                <span>312</span>
+                <span class="material-symbols-outlined icon">
+                  contacts_product
+                </span>
+                312
               </div>
               <div className="rang">
-                <span class="material-symbols-outlined">library_add_check</span>
-                <span>100</span>
+                <span class="material-symbols-outlined icon">
+                  library_add_check
+                </span>
+                100
               </div>
             </div>
             <button>Follow +</button>
