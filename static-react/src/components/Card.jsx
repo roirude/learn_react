3aa6625d3166f07a16d62a7a8dd9395a08747d93
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-02 05:26:29 +0100

    Create sementic html

--- static-react/src/components/Card.jsx
+++ static-react/src/components/Card.jsx
@@ -2,20 +2,29 @@ export default function Card() {
   return (
     <div className="card">
       <div className="card-content">
-        <h1>Rudex J. Tede</h1>
-        <p>React Developer with a passion for building web applications.</p>
-        <div className="card-footer">
-          <div className="card-rang">
-            <div>
-              <span class="material-symbols-outlined">contacts_product</span>
-              <span>312</span>
-            </div>
-            <div>
-              <span class="material-symbols-outlined">library_add_check</span>
-              <span>100</span>
+        <div className="cloud-container">
+          <div className="cloud-filter"></div>
+          <div className="cloud-content">
+            <h1>Rudex J. Tede</h1>
+            <p>React Developer with a passion for building web applications.</p>
+            <div className="card-footer">
+              <div className="card-rang">
+                <div>
+                  <span class="material-symbols-outlined">
+                    contacts_product
+                  </span>
+                  <span>312</span>
+                </div>
+                <div>
+                  <span class="material-symbols-outlined">
+                    library_add_check
+                  </span>
+                  <span>100</span>
+                </div>
+              </div>
+              <button>Follow +</button>
             </div>
           </div>
-          <button>Follow +</button>
         </div>
       </div>
     </div>
