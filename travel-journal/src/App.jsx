1f312fcc1bd75bd2a6a384cac06a4a24c79611a2
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-05 19:56:43 +0100

    Completed New challenge. using maps to render every item in the data array

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,39 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./data";
 
 /**
- * Challenge: pass props to the Entry component to display
- * its data. See the `data.md` file for each prop name and its
- * value.
- *
- * Then on the Entry component, receive and display the values
- * for those props. In the end, the page should look the same
- * as it does now, but without all the hard-coded data in the
- * component
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
  */
 
 function App() {
-  const img = {
-    src: "https://scrimba.com/links/travel-journal-japan-image-url",
-    altImd: "Mount Fuji",
-  };
-
   return (
     <>
       <main>
         <Header />
-        <Entry
-          img={img}
-          title="Mount Fuji"
-          country="japan"
-          googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-          dates="12 Jan, 2021 - 24 Jan, 2021"
-          text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        />
+        {
+          data.map((entry) => (
+            <Entry
+              img={{
+                src: entry.img.src,
+                alt: entry.img.alt
+              }}
+              title={entry.title}
+              country={entry.country}
+              googleMapsLink={entry.googleMapsLink}
+              dates={entry.dates}
+              text={entry.text}
+            />
+          ))
+        }
       </main>
     </>
   );
