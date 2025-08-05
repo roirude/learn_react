e102c3c469ec351fbaa359d56fd72e5e1f916630
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-05 18:30:58 +0100

    Complete props challenge

--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -14,26 +14,26 @@
  *   this data into the component.
  */
 
-export default function Entry() {
+export default function Entry(props) {
   return (
     <article>
       <div className="main-image-container">
-        <img className="main-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount Fuji" />
+        <img className="main-image" src={props.img.src} alt={props.img.alt} />
       </div>
       <div>
         <p className="location">
           <img src="src/assets/marker.png" alt="Location mark" />
-          <span>Japan</span>
-          <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+          <span>{props.country}</span>
+          <a href={props.googleMapsLink}>
             View on Google Maps
           </a>
         </p>
-        <h2 className="title">Mount Fuji</h2>
-        <p className="date">12 Jan, 2021 - 24 Jan, 2021</p>
+        <h2 className="title">{ props.title }</h2>
+        <p className="date">{ props.dates }</p>
         <p className="description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+          {
+            props.text
+          }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
         </p>
       </div>
     </article>
