fed231d7b6745a9fc95989a3b1e1f1bcdc0435bd
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-05 23:03:31 +0100

    Completed chalenge and create Header component

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import chefClaudeIcon from '../assets/chef-claude-icon.png'
+
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeIcon} alt="Chef Claude Icon" />
+            <h1>Chef claude</h1>
+        </header>
+    )
+}
