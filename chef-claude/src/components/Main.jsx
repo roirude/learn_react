7e8d89ee1cefacb29c1f7c55d56f535fe7434b76
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-06 15:46:10 +0100

    Completed new challenge

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,14 +1,31 @@
 export default function Main() {
-    return (
-        <main>
-            <form className="add-ingredient-form">
-                <input 
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
-        </main>
-    )
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  /**
+   * Review Challenge:
+   * Map over the list of ingredients and render them as list items
+   *
+   * Note: We're doing things a weird way here. Don't worry,
+   * we're building up to learning the right way 🙂
+   */
+
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>
+        {
+            ingredients.map((ingredient) => (
+                <li key={ingredient}>{ ingredient }</li>
+            ))
+        }
+      </ul>
+    </main>
+  );
 }
