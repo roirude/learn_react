4fedbc4afdeb5ee3729e448aaff8d13c92658946
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-23 21:40:20 +0100

    Refactor code and complete new challenge about conditional rendering

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import React from "react";
 
 export default function Main() {
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = React.useState([]);
 
-      /**
-     * Challenge: use form action instead of onSubmit to
-     * handle the data from the form
-     */
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
 
   function addIngredient(formData) {
-    const newIngredient = formData.get("ingredient")
-    setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
+
+  /**
+   * Challenge:
+   * Using conditional rendering, only render the new <section> IF
+   * there are ingredients added to the list of ingredients.
+   */
 
   return (
     <main>
@@ -24,11 +29,21 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>
-        {ingredients.map((ingredient) => (
-          <li key={ingredient}>{ingredient}</li>
-        ))}
-      </ul>
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          <div className="get-recipe-container">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button>Get a recipe</button>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
