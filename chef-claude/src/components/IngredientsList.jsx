deaa5597f921918041cef25cc159b8e89667d9e2
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-25 23:13:04 +0100

    complete refactor to separate components challenge

---
+++ chef-claude/src/components/IngredientsList.jsx
@@ -0,0 +1,22 @@
+export default function IngredientsList({ ingredients, toggle }) {
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {ingredientsListItems}
+      </ul>
+      {ingredients.length > 3 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={toggle}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+}
