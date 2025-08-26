a329a87cb933dbcbeb7578dff565bc1952c7eef7
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-26 20:07:47 +0100

    Create meme generator project for section 4 learn react course on scrimba

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+import "./App.css"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
