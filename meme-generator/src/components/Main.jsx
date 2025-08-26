a189837339f38c2027ac8d9d7493707aaeda64a9
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-26 20:31:19 +0100

    Complete controlled components part 1 challenge's

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,43 +1,55 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Main() {
-    const [meme, setMeme] = useState({
-        topText: "One does not simply",
-        bottomText: "Walk into Mordor",
-        imageUrl: "http://i.imgflip.com/1bij.jpg"
-    })
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  function handleChange(event) {
+    const { value } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      topText: value,
+    }));
     /**
-     * Challenge: move the hardcoded meme info into React
-     * state. Use an object with `topText`, `bottomText`,
-     * and `imageUrl` properties, and set the initial values to
-     * the ones hardcoded below.
+     * Challenge: update the topText value in the meme state
+     * object every time the topText input box is changed
+     *
+     * Note: don't worry about bottomText at this point.
      */
+  }
 
-    return (
-        <main>
-            <div className="form">
-                <label>Top Text
-                    <input
-                        type="text"
-                        placeholder="One does not simply"
-                        name="topText"
-                    />
-                </label>
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+          />
+        </label>
 
-                <label>Bottom Text
-                    <input
-                        type="text"
-                        placeholder="Walk into Mordor"
-                        name="bottomText"
-                    />
-                </label>
-                <button>Get a new meme image 🖼</button>
-            </div>
-            <div className="meme">
-                <img src={ meme.imageUrl } />
-                <span className="top">{ meme.topText }y</span>
-                <span className="bottom">{ meme.bottomText }</span>
-            </div>
-        </main>
-    )
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+          />
+        </label>
+        <button>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
 }
