f0f05d65bcb2f101626f0259005f640a8563466c
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-27 04:10:08 +0100

    Create Die component and style it

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
+import Die from "./components/Die";
 
 function App() {
-
   return (
     <main>
-      
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={3} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={4} />
+        <Die value={2} />
+        <Die value={5} />
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
